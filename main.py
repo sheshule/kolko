e9f238e1ed59e5c03b6262d58bc37577ff901f23
sheshule <sheshule@gmail.com>
2021-12-18 12:05:56 +0100

    całość

--- main.py
+++ main.py
@@ -16,5 +16,85 @@ def drukujPlansze(pole):
     print(f"{pole['4']}|{pole['5']}|{pole['6']}")
     print('-+-+-')
     print(f"{pole['1']}|{pole['2']}|{pole['3']}")
-drukujPlansze(PlanszaDoGry)
+#drukujPlansze(PlanszaDoGry)
 
+def gra():
+    licznik = 0
+    gracz = 'x'
+
+    for i in range(9):
+        drukujPlansze(PlanszaDoGry)
+        ruch = input(f'Rozpoczyna {gracz}. Wybierz, gdzie postawić znak.')
+
+        if PlanszaDoGry[ruch] == ' ':
+            PlanszaDoGry[ruch] = gracz
+            licznik += 1
+        else:
+            print('Miejsce zajęte!\nWybierz inne miejsce!')
+            continue
+        
+        if licznik >= 5:
+            # poziome
+            if PlanszaDoGry['7'] == PlanszaDoGry['8'] == PlanszaDoGry['9']  != ' ':
+                drukujPlansze(PlanszaDoGry)
+                print('\nKONIEC GRY!')
+                print(f'Wygrał gracz: {gracz}\n')
+                break
+            if PlanszaDoGry['4'] == PlanszaDoGry['5'] == PlanszaDoGry['6']  != ' ':
+                drukujPlansze(PlanszaDoGry)
+                print('\nKONIEC GRY!')
+                print(f'Wygrał gracz: {gracz}\n')
+                break
+            if PlanszaDoGry['1'] == PlanszaDoGry['2'] == PlanszaDoGry['3']  != ' ':
+                drukujPlansze(PlanszaDoGry)
+                print('\nKONIEC GRY!')
+                print(f'Wygrał gracz: {gracz}\n')
+                break
+            # pionowe
+            if PlanszaDoGry['7'] == PlanszaDoGry['4'] == PlanszaDoGry['1']  != ' ':
+                drukujPlansze(PlanszaDoGry)
+                print('\nKONIEC GRY!')
+                print(f'Wygrał gracz: {gracz}\n')
+                break
+            if PlanszaDoGry['8'] == PlanszaDoGry['5'] == PlanszaDoGry['2']  != ' ':
+                drukujPlansze(PlanszaDoGry)
+                print('\nKONIEC GRY!')
+                print(f'Wygrał gracz: {gracz}\n')
+                break
+            if PlanszaDoGry['9'] == PlanszaDoGry['6'] == PlanszaDoGry['3']  != ' ':
+                drukujPlansze(PlanszaDoGry)
+                print('\nKONIEC GRY!')
+                print(f'Wygrał gracz: {gracz}\n')
+                break
+            # ukośne
+            if PlanszaDoGry['1'] == PlanszaDoGry['5'] == PlanszaDoGry['9']  != ' ':
+                drukujPlansze(PlanszaDoGry)
+                print('\nKONIEC GRY!')
+                print(f'Wygrał gracz: {gracz}\n')
+                break
+            if PlanszaDoGry['7'] == PlanszaDoGry['5'] == PlanszaDoGry['3']  != ' ':
+                drukujPlansze(PlanszaDoGry)
+                print('\nKONIEC GRY!')
+                print(f'Wygrał gracz: {gracz}\n')
+                break
+
+            if licznik == 9:
+                drukujPlansze(PlanszaDoGry)
+                print('\nKONIEC GRY!')
+                print('\nREMIS\n')
+            
+        if gracz == 'x':
+            gracz = 'o'
+        else:
+            gracz = 'x'
+
+    restart = input('Czy chcesz zagrać ponownie? odp: t/n')
+    if restart == 't' or restart == 'T':
+        for key in KlawiszeDoGry:
+            PlanszaDoGry[key] = ' '
+        
+        gra()
+
+if __name__ == '__main__':
+    gra()
+            
